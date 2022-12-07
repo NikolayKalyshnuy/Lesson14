70f890a111b966f21a4f34dd0a2ed4935efa063e
NikolayKalyshnuy <nikkalaba245@gmail.com>
2022-12-07 21:01:26 +0200

    Add metods to class Car

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 class Car:
-    def __init__(self, type, kp, motor):
+    def __init__(self, name, type, kp, motor):
+        self.name = name
         self.type = type
         self.kp1 = kp
         self.motor = motor
+        self.numberWell = 4
 
-car1 = Car("HB", "Auto", "Electric")
-car2 = Car("Sedan", "Manual", "Dizel")
+    def move(self, speed):
+        print(f"move {self.name} {speed}km/h")
 
-print(car1)
+    def signal(self):
+        print(f'{self.name} beep-beep')
+
+car1 = Car("Tesla","HB", "Auto", "Electric")
+car2 = Car("Porshe","Sedan", "Manual", "Dizel")
+
+car1.move(250)
+car2.signal()
+
+print(car1.kp1)
+print(car2.motor)
